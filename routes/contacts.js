50a0474b743d896265f668fbc4bb67562b69f67a
Your Name <yourgithubemail@email.com>
2026-06-05 17:33:44 -0600

    Add protected routes with OAuth authentication

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,11 +2,15 @@ const router = require('express').Router();
 
 const controller = require('../controllers/contacts');
 const validate = require('../middleware/validateContact');
+const isAuthenticated = require('../middleware/authenticate');
 
+// Public Routes
 router.get('/', controller.getAllContacts);
 router.get('/:id', controller.getContact);
-router.post('/', validate, controller.createContact);
-router.put('/:id', validate, controller.updateContact);
-router.delete('/:id', controller.deleteContact);
+
+// Protected Routes
+router.post('/', isAuthenticated, validate, controller.createContact);
+router.put('/:id', isAuthenticated, validate, controller.updateContact);
+router.delete('/:id', isAuthenticated, controller.deleteContact);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,16 +2,19 @@ const router = require('express').Router();
 
 const controller = require('../controllers/users');
 const validate = require('../middleware/validateUser');
+const isAuthenticated = require('../middleware/authenticate');
 
 // Authentication Routes
 router.post('/register', controller.registerUser);
 router.post('/login', controller.loginUser);
 
-// User CRUD Routes
+// Public Routes
 router.get('/', controller.getAllUsers);
 router.get('/:id', controller.getUser);
-router.post('/', validate, controller.createUser);
-router.put('/:id', validate, controller.updateUser);
-router.delete('/:id', controller.deleteUser);
+
+// Protected Routes
+router.post('/', isAuthenticated, validate, controller.createUser);
+router.put('/:id', isAuthenticated, validate, controller.updateUser);
+router.delete('/:id', isAuthenticated, controller.deleteUser);
 
 module.exports = router;
